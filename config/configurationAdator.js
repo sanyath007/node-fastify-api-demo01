489e63eeb0bb92500eeb4b8db6e9cf55fc076aaf
sanyath007 <sanyath007@gmail.com>
2020-12-22 21:31:30 +0700

    Remove ignoring config folder from .gitignore

---
+++ config/configurationAdator.js
@@ -0,0 +1,23 @@
+const nconf = require('nconf');
+const _ = require('lodash');
+const { reject } = require('lodash');
+
+const loadSettings = ({ appSettingsPath }) => new Promise((resolve, reject) => {
+    try {
+        if (_.isEmpty(appSettingsPath)) {
+            throw new Error('Configuration settings path is required.');
+        }
+
+        nconf.file({
+            file: appSettingsPath,
+            // setting the separator as dot for nested objects
+            logicalSeparator: '.',
+        });
+        
+        resolve();
+    } catch (err) {
+        reject(err);
+    }
+});
+
+module.exports.loadSettings = loadSettings;
